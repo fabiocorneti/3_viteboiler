6ce6a222f8993acb5aa4402e59dd658bd3b61bec
Fabio Corneti <info@corneti.com>
2026-02-08 06:28:58 +0000

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import { useState, useEffect } from "react";
-
 export default function App() {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
-
   return (
     <div>
       <h1>Hello</h1>
